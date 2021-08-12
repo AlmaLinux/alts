6fc59c41d2218e03f53b28f81270cf7fc6033a68
Vasily Kleschov <vkleschov@almalinux.org>
2021-08-12 15:07:58 +0300

    Fixed Celery queues setup

--- alts/shared/constants.py
+++ alts/shared/constants.py
@@ -1,6 +1,9 @@
 
-__all__ = ['API_VERSION']
+__all__ = ['API_VERSION', 'ARCHITECTURES', 'COSTS', 'DRIVERS']
 
 
 # YYYYMMDD format for API version
 API_VERSION = '20210512'
+COSTS = [str(i) for i in range(5)]
+ARCHITECTURES = ['x86_64', 'x86', 'aarch64']
+DRIVERS = ['docker', 'opennebula']
