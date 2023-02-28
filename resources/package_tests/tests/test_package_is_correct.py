d3a4e70fba6d51442e620df596f723e42e1d1fa8
Vasily Kleschov <kleshev12@gmail.com>
2023-02-28 20:16:51 +0100

    ALBS-1017: Fix integrity tests when it comes to docs

--- resources/package_tests/tests/test_package_is_correct.py
+++ resources/package_tests/tests/test_package_is_correct.py
@@ -25,10 +25,17 @@ def test_package_is_installed(host, package_name, package_version):
     package = host.package(package_name)
     check.is_true(package.is_installed)
     if package_version:
-        check.is_in(package.version, package_version, 'Version does not match')
+        check.is_in(
+            package.version, package_version,
+            'Version does not match: required: {0}, actual: {1}'.format(
+                package_version, package.version)
+        )
         if isinstance(package, RpmPackage):
-            check.is_in(package.release, package_version,
-                        'Release does not match')
+            check.is_in(
+                package.release, package_version,
+                'Release does not match: required: {0}, actual: {1}'.format(
+                    package_version, package.release)
+            )
 
 
 def test_all_package_files_exist(host, package_name):
@@ -55,13 +62,18 @@ def test_all_package_files_exist(host, package_name):
 
     for file_ in get_package_files(package):
         file_obj = host.file(file_)
-        check.is_true(file_obj.exists)
+        check.is_true(
+            file_obj.exists,
+            'File is absent on the file system: {}'.format(file_)
+        )
         # Check all symlinks point to existing files
         if file_obj.is_symlink:
             resolved = resolve_symlink(host, file_obj)
-            check.is_not_none(resolved)
+            check.is_not_none(
+                resolved, 'Symlink cannot be resolved: {}'.format(file_))
             file_obj = host.file(resolved)
-            check.is_true(file_obj.exists)
+            check.is_true(
+                file_obj.exists, 'Symlink is broken: {}'.format(file_))
 
 
 def test_binaries_have_all_dependencies(host, package_name):
@@ -118,4 +130,5 @@ def test_check_rpath_is_correct(host, package_name):
         return
 
     for library in get_shared_libraries(package):
-        check.is_true(is_rpath_correct(host.file(library)))
+        check.is_true(is_rpath_correct(host.file(library)),
+                      'RPATH is broken in {}'.format(library))
