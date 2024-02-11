0d38fcb7d35fbed6dbb27ae525fcc805e55a2f15
Vasily Kleschov <kleshev12@gmail.com>
2024-02-11 18:26:02 +0100

    Reworked git clone, added --depth=1 parameter

--- alts/shared/utils/git_utils.py
+++ alts/shared/utils/git_utils.py
@@ -7,26 +7,64 @@ from plumbum import local
 from alts.shared.utils.path_utils import get_abspath
 
 
-def checkout(git_ref: str, git_repo_path: Path, logger: Logger):
+def checkout(
+    git_ref: str,
+    git_repo_path: Path,
+    logger: Logger,
+    cmd_timeout: int = 300,
+):
     logger.debug('Switching to the git branch/tag: %s', git_ref)
-    exit_code, _, stderr = local['bash'].run(
+    exit_code, _, stderr = local['bash'].with_cwd(git_repo_path).run(
         ['-c', f'git fetch origin && git checkout {git_ref}'],
         retcode=None,
-        cwd=git_repo_path,
+        timeout=cmd_timeout,
     )
 
 
 def git_reset_hard(git_repo_path: Path, logger: Logger):
-    exit_code, _, stderr = local['bash'].run(
+    exit_code, _, stderr = local['bash'].with_cwd(git_repo_path).run(
         ['-c', 'git checkout master && git reset --hard origin/master'],
         retcode=None,
-        cwd=git_repo_path,
     )
     if exit_code != 0:
         logger.error(
             'Cannot reset the git index and working tree:\n%s',
             stderr,
         )
+
+
+def __clone_git_repo(
+    repo_url: str,
+    work_dir: Path,
+    logger: Logger,
+    reference_directory: Optional[str] = None,
+    cmd_timeout: int = 300,
+):
+    git_repo_path = Path(
+        work_dir,
+        Path(repo_url).name.replace('.git', ''),
+    )
+    if git_repo_path.exists():
+        return git_repo_path
+    logger.debug('Cloning the git repo: %s', repo_url)
+    args = ['clone', repo_url, '--depth', '1']
+    if reference_directory:
+        args.extend(
+            ['--reference-if-able', get_abspath(reference_directory)]
+        )
+    exit_code, _, stderr = local['git'].with_cwd(work_dir).run(
+        args,
+        retcode=None,
+        timeout=cmd_timeout,
+    )
+    if exit_code != 0:
+        logger.error(
+            'Cannot clone the git repo: %s\n%s',
+            repo_url,
+            stderr,
+        )
+        return
+    return git_repo_path
 
 
 def clone_git_repo(
@@ -37,34 +75,15 @@ def clone_git_repo(
     reference_directory: Optional[str] = None,
     cmd_timeout: int = 300,
 ) -> Optional[Path]:
-    git_repo_path = Path(
+    git_repo_path = __clone_git_repo(
+        repo_url,
         work_dir,
-        Path(repo_url).name.replace('.git', ''),
+        logger,
+        reference_directory=reference_directory,
+        cmd_timeout=cmd_timeout,
     )
-    if git_repo_path.exists():
-        logger.debug('The git repo %s has already been cloned', repo_url)
+    if git_repo_path:
         checkout(git_ref, git_repo_path, logger)
-        return git_repo_path
-    logger.debug('Cloning the git repo: %s', repo_url)
-    args = ['clone', repo_url]
-    if reference_directory:
-        args.extend(
-            ['--reference-if-able', get_abspath(reference_directory)]
-        )
-    exit_code, _, stderr = local['git'].run(
-        args,
-        retcode=None,
-        cwd=work_dir,
-        timeout=cmd_timeout,
-    )
-    if exit_code != 0:
-        logger.error(
-            'Cannot clone the git repo: %s\n%s',
-            repo_url,
-            stderr,
-        )
-        return
-    checkout(git_ref, git_repo_path, logger)
     return git_repo_path
 
 
@@ -98,37 +117,28 @@ def clone_gerrit_repo(
     cmd_timeout: int = 300,
 ) -> Optional[Path]:
     # ssh://gerrit.test.com:00000/repo
-    git_repo_path = Path(work_dir, Path(repo_url).name)
-    if not git_repo_path.exists():
-        logger.debug('Cloning the git repo: %s', repo_url)
-        args = ['clone', repo_url]
-        if reference_directory:
-            args.extend(
-                ['--reference-if-able', get_abspath(reference_directory)]
-            )
-        exit_code, _, stderr = local['git'].run(
-            args,
+    git_repo_path = __clone_git_repo(
+        repo_url,
+        work_dir,
+        logger,
+        reference_directory=reference_directory,
+        cmd_timeout=cmd_timeout,
+    )
+    if git_repo_path:
+        gerrit_command = prepare_gerrit_command(git_ref)
+        if not gerrit_command:
+            logger.debug('Nothing to do, skipping')
+            return
+        exit_code, _, stderr = local['bash'].with_cwd(git_repo_path).run(
+            ['-c', gerrit_command],
             retcode=None,
-            cwd=work_dir,
             timeout=cmd_timeout,
         )
         if exit_code != 0:
-            logger.error('Cannot clone the git repo: %s\n%s', repo_url, stderr)
+            logger.error(
+                'Cannot execute gerrit command: %s\n%s',
+                gerrit_command,
+                stderr,
+            )
             return
-    gerrit_command = prepare_gerrit_command(git_ref)
-    if not gerrit_command:
-        logger.debug('Nothing to do, skipping')
-        return
-    exit_code, _, stderr = local['bash'].run(
-        ['-c', gerrit_command],
-        retcode=None,
-        cwd=git_repo_path,
-    )
-    if exit_code != 0:
-        logger.error(
-            'Cannot execute gerrit command: %s\n%s',
-            gerrit_command,
-            stderr,
-        )
-        return
     return git_repo_path
