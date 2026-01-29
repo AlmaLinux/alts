4545228ca8993306bb4dd4c675071be98c97b7ac
Daniil Anfimov <anfimovdan@gmail.com>
2026-01-29 12:20:28 +0200

    Fix "Repositories details" command for Debian systems

--- alts/worker/runners/base.py
+++ alts/worker/runners/base.py
@@ -800,8 +800,8 @@ class BaseRunner(object):
             basic_commands['Installed packages'] = ('dpkg', '-l')
             basic_commands['Repositories list'] = ('apt-cache', 'policy')
             basic_commands['Repositories details'] = (
-                'find', '/etc/apt/', '-type', 'f', '(', '-name', '*.list*',
-                '-o', '-name', '*.sources*', ')', '-exec', 'cat', '{}', '+'
+                'find', '/etc/apt/', '-type', 'f', r'\(', '-name', "'*.list*'",
+                '-o', '-name', "'*.sources*'", r'\)', '-exec', 'cat', '{}', '+'
             )
         return basic_commands
 
