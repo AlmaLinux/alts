9ca90467956eea68462a100e5aecf2773cab3398
Daniil Anfimov <64651799+anfimovdm@users.noreply.github.com>
2026-03-11 11:07:02 +0200

    Add ability to pull docker images from a custom registry (#200)

--- alts/shared/models.py
+++ alts/shared/models.py
@@ -344,6 +344,7 @@ class CeleryConfig(BaseModel):
     git_reference_directory: Optional[str] = None
     tests_base_dir: str = '/tests'
     package_proxy: str = ''
+    docker_registry_prefix: str = ''
     development_mode: bool = False
     authorize_build_repositories: bool = False
 
